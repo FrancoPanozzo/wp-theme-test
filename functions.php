02b0590771c8887ed34ce1e829aef2db54752fa4
Franco <francopanozzo@hotmail.com.ar>
2020-12-18 14:32:10 +0100

    Added dynamic title tag via the wp api

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
 
+// Add dynamic title tag support in the content manager UI from WP CMS
+function add_all_theme_supports(){
+add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'add_all_theme_supports');
+
+
+// Enqueue style tags
 function registerStyles(){
     $version = wp_get_theme()->get('Version');
     wp_enqueue_style('main-stylesheet', get_template_directory_uri() . './style.css', array('bootstrap-css-cdn'), $version, 'all');
@@ -9,6 +18,7 @@ function registerStyles(){
 
 add_action('wp_enqueue_scripts', 'registerStyles');
 
+// Enqueue javascript tags
 function registerScripts(){
     wp_enqueue_script('jquery-cdn', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
     wp_enqueue_script('popper-cdn', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.0',  true);
